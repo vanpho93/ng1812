b11e92495a16f39c98e095afe111e31b94fa8c47
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-14 21:16:44 +0700

    xong

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,24 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   words: Word[] = [
-    { en: 'One', vn: 'Mot', isMemorized: true },
-    { en: 'Two', vn: 'Hai', isMemorized: false },
-    { en: 'Three', vn: 'Ba', isMemorized: false },
-    { en: 'Four', vn: 'Bon', isMemorized: true }
+    { en: 'One', vn: 'Mot', isMemorized: true, _id: 'abcd1' },
+    { en: 'Two', vn: 'Hai', isMemorized: false, _id: 'abcd2' },
+    { en: 'Three', vn: 'Ba', isMemorized: false, _id: 'abcd3' },
+    { en: 'Four', vn: 'Bon', isMemorized: true, _id: 'abcd4' }
   ];
+
+  removeWord(_id: string) {
+    const index = this.words.findIndex(word => word._id === _id);
+    this.words.splice(index, 1);
+  }
+
+  toggleWord(_id: string) {
+    const word = this.words.find(word => word._id === _id);
+    word.isMemorized = !word.isMemorized;
+  }
 }
 interface Word {
+  _id: string;
   en: string;
   vn: string;
   isMemorized: boolean;
