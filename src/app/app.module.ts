e55e913ecca63a0ad19ab37600ba3dbdef5949f3
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-14 20:47:28 +0700

    html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { WordComponent } from './word.component';
+import { ListWordComponent } from './list-word/list-word.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WordComponent
+    WordComponent,
+    ListWordComponent
   ],
   imports: [
     BrowserModule
--- src/app/word.component.ts
+++ src/app/word.component.ts
@@ -2,12 +2,30 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-word',
-    template: '<h3>Word Component 123</h3>',
+    template: `
+        <div>
+            <h4 style="color: red" *ngIf="!isMemorized">{{ en }}</h4>
+            <h4 style="color: green" *ngIf="isMemorized">{{ en }}</h4>
+            <p>{{ vn }}</p>
+            <button
+                class="{{ isMemorized ? 'btn btn-danger' : 'btn btn-success' }}"
+                (click)="toggleIsMemorized();"
+            >
+                Toggle
+            </button>
+        </div>
+    `,
     styles: [
         'h3 { color: blue }'
     ]
 })
 
-export class WordComponent {}
+export class WordComponent {
+    en = 'One';
+    vn = 'Mot';
+    isMemorized = true;
+
+    toggleIsMemorized() { this.isMemorized = !this.isMemorized }
+}
 
 // modules.export = { WordComponent }
