c83bea9ab11ae7d1cd175e10a07d633b969de4d8
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-14 20:13:35 +0700

    abc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'app 123';
+  num = 10;
+  increase() { this.num++; }
+  descrease() { this.num--; }
+  reset() { this.num = 0; }
 }
---
+++ src/app/word.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-word',
+    template: '<h3>Word Component 123</h3>',
+    styles: [
+        'h3 { color: blue }'
+    ]
+})
+
+export class WordComponent {}
+
+// modules.export = { WordComponent }
