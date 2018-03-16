955f305ff01b51d827497119f825caa70b9b176e
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-16 21:01:09 +0700

    word

--- src/app/word.component.ts
+++ src/app/word.component.ts
@@ -2,7 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-word',
-    template: ``
+    template: `
+        <div>
+            <h4 class="text-success">
+              One
+            </h4>
+            <p>Mot</p>
+            <button class="btn btn-danger">
+              Remove
+            </button>
+            <button class="btn btn-success">
+              Toggle
+            </button>
+        </div>
+    `
 })
 
 export class WordComponent {}
