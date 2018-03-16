aba20aac64142e2a4971c88c7f85e6ac3628cf25
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-16 20:51:36 +0700

    binding class and binding style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { WordComponent } from './word.component';
-import { ListWordComponent } from './list-word/list-word.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WordComponent,
-    ListWordComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
