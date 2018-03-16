9612c58a544bd2fa916983da6bd4f59d3d52b50f
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-16 20:55:52 +0700

    word - component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { WordComponent } from './word.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WordComponent
   ],
   imports: [
     BrowserModule,
